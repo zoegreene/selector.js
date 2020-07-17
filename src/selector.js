5be807350ec450d5e2b3d3b35c3e4908beb81ba1
Mark Guinn <mxavier927@gmail.com>
2020-07-16 21:06:41 -0400

    Mark's changes

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,24 @@
-// YOUR CODE HERE
+class $ {
+    constructor(selector) {
+    if (selector[0] === '.') {
+        this.elements = Array.from(document.getElementsByClassName(selector.slice(1)))
+    } else if (selector[0] === '#') {
+        this.elements = [document.getElementById(selector.slice(1))]
+    } else {
+        this.elements = Array.from(document.getElementsByTagName(selector))
+    }
+}
+    hide() {
+        this.elements.forEach( elem => {
+            elem.style.display = 'none'
+        }) 
+    }
+
+    show() {
+        this.elements.forEach( elem => {
+            elem.style.display = 'inherit'
+        })
+    }
+
+    addClassName() {}
+}

945a59e47b451c96e5055ca39897d1e4988cc477
Zoe Greene <zoe.greene12@gmail.com>
2020-07-17 17:15:49 -0700

    Completed all exercises

--- src/selector.js
+++ src/selector.js
@@ -1,24 +1,51 @@
+// see if you can change the constructor to handle longer selector strings
+
 class $ {
-    constructor(selector) {
+  constructor(selector) {
     if (selector[0] === '.') {
-        this.elements = Array.from(document.getElementsByClassName(selector.slice(1)))
+      this.elements = Array.from(document.getElementsByClassName(selector.slice(1)))
     } else if (selector[0] === '#') {
-        this.elements = [document.getElementById(selector.slice(1))]
+      this.elements = [document.getElementById(selector.slice(1))]
     } else {
-        this.elements = Array.from(document.getElementsByTagName(selector))
+      this.elements = Array.from(document.getElementsByTagName(selector))
     }
-}
-    hide() {
-        this.elements.forEach( elem => {
-            elem.style.display = 'none'
-        }) 
-    }
+  }
 
-    show() {
-        this.elements.forEach( elem => {
-            elem.style.display = 'inherit'
-        })
-    }
+  iterator(func) {
+    this.elements.forEach(elem => {
+      func(elem);
+    });
+  }
 
-    addClassName() {}
+  hide() {
+    this.iterator(elem => elem.style.display = 'none');
+    console.log(this.elements)
+    return this;
+  }
+
+  show() {
+    this.iterator(elem => elem.style.display = 'inherit');
+    return this;
+  }
+
+  addClassName(className) {
+    this.iterator(elem => elem.classList.add(className));
+    return this;
+  }
+
+  removeClassName(className) {
+    this.iterator(elem => elem.classList.remove(className));
+    return this;
+  }
+
+  text(textNode) {
+    this.iterator(elem => elem.innerText = textNode);
+    return this;
+  }
+
+  addChild(nodeType) {
+    this.iterator(elem => elem.appendChild(document.createElement(nodeType)));
+    return this;
+  }
+
 }
